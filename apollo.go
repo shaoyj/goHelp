87b79005db821373771a9bab12dd7381f0717dcf
邵永军 <shaoyongjun@shouqianba.com>
2019-05-07 16:47:44 +0800

    add apollo 002

--- apollo.go
+++ apollo.go
@@ -4,12 +4,12 @@ import (
 	"github.com/shima-park/agollo"
 )
 
-var apollo agollo.Agollo
+var Apollo agollo.Agollo
 
 func InitApollo(configServerURL, appID string) {
 
 	var err error
-	apollo, err = agollo.New(
+	Apollo, err = agollo.New(
 		configServerURL,
 		appID,
 		agollo.AutoFetchOnCacheMiss(),
